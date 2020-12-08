f76271a7906cc0455deacf18bdef118e00d23e6c
Elvira-Camarillo <gcamarilloluna@gmail.com>
2020-12-07 21:07:49 -0600

    ADD class one:octocat:

---
+++ class-one/js/main.js
@@ -0,0 +1,23 @@
+console.log('Hola Koders')
+
+// Tipos de variables
+// var, let & const
+
+//Formas de definir variables.
+var nombreVariable;
+let nombreVariable2
+let nombreVariable3 = 'Algo'
+
+let numero = 1000
+let boleano = true
+let myArray = [1, '2', true]
+let myObjectKoder = {
+    nombre = 'Elvi'
+}
+
+const PI = 3.1416 //Los nombres de las constantes se deben escribir en mayúsculas.
+
+//Formas de escritura para el curso
+
+let name_koder = 'Jorge'
+const NAME_KODER = 'Oscar'
