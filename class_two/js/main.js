3652d27c944077cd33de818ea5da53df9f8740d2
Elvira-Camarillo <gcamarilloluna@gmail.com>
2020-12-08 21:52:10 -0600

    ADD ternary operator in main.js:octocat:

--- class_two/js/main.js
+++ class_two/js/main.js
@@ -1,11 +1,20 @@
 console.log('Hola Koders')
 
-let age = 10
-// age = age + 1 Forma larga 
-age += 1  // Forma rápida
+// let age = 10
+// // age = age + 1 Forma larga 
+// age += 1  // Forma rápida
 
-console.log(age)
+// console.log(age)
 
+console.log('Condicional: IF Ternario')
+// If ternario 
+// expression ? si true : false
 
+// Comparar si el primer número es mayor que el segundo
+const NUMBER_ONE = parseInt(prompt('Escribe un número'))
+const NUMBER_TWO = parseInt(prompt('Escribe un segundo número diferente'))
 
-
+NUMBER_ONE === NUMBER_TWO ? 
+    console.log(`El ${NUMBER_ONE} es el mayor`)
+    :
+    console.log(`El ${NUMBER_TWO} es el mayor`)
