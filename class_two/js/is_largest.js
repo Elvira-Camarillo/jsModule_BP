7b82adfcf6af1d2506411489faebf30caa2350a2
Elvira-Camarillo <gcamarilloluna@gmail.com>
2020-12-08 21:06:30 -0600

    ADD weather_today.js

--- class_two/js/is_largest.js
+++ class_two/js/is_largest.js
@@ -1,6 +1,9 @@
 console.log('¿Qué número es mayor?')
-const NUMBER_ONE = prompt('Dame un número por favor')
-const NUMBER_TWO = prompt('Escribe un segundo número')
+
+const NUMBER_ONE = parseInt(prompt('Dame un número por favor')) 
+
+const NUMBER_TWO = parseInt(prompt('Escribe un segundo número'))
+
 if (NUMBER_ONE > NUMBER_TWO) {
     console.log(`El ${NUMBER_ONE} es el mayor`)
 } else if (NUMBER_ONE === NUMBER_TWO) {
---
+++ class_two/js/weather_today.js
@@ -0,0 +1,15 @@
+console.log('Ejercicio 2: ¿cómo esta el clima hoy?')
+let weather_today = prompt('¿cómo esta el día hoy?')
+weather_today = weather_today.toLocaleLowerCase()
+if( weather_today == 'soleado' ) {
+    console.log(`El día esta soleado por acá`)
+} else if ( weather_today == 'lluvioso' ) {
+    console.log(`El día esta lluvioso por acá`)
+} else if (  weather_today == 'nevando' ) {
+    console.log(`Hoy esta nevando por acá`)
+} else if ( weather_today == 'nublado' ) {
+    console.log(`El día esta nublado por acá`)
+} else {
+    let weather_description = prompt('Me podrías indicar el clima por favor')
+    console.log(`El clima esta: ${weather_description} hoy`)
+}
