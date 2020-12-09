1f02ee7f17c5ee6927ff81e54608bcbe721e0f1d
Elvira-Camarillo <gcamarilloluna@gmail.com>
2020-12-08 19:29:35 -0600

    ADD weight_in_moonoctocat:

---
+++ class-one/js/weight_in_moon.js
@@ -0,0 +1,8 @@
+// Ejercicio Clase 1
+
+let weightKoder
+weightKoder = prompt('¿Cuál es tu peso en Kg?')
+
+weight_koder_in_moon = (weightKoder * 1.622)/9.8
+
+alert(`Tú peso en la luna es: ${weight_koder_in_moon.toFixed(3)} Kg`)
