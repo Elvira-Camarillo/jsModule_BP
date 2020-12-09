9cc4eea9eb72417dec81e4d82dca85475f7715f5
Elvira-Camarillo <gcamarilloluna@gmail.com>
2020-12-08 19:10:12 -0600

    MOD main.js

--- class-one/js/main.js
+++ class-one/js/main.js
@@ -3,21 +3,32 @@ console.log('Hola Koders')
 // Tipos de variables
 // var, let & const
 
-//Formas de definir variables.
-var nombreVariable;
-let nombreVariable2
-let nombreVariable3 = 'Algo'
+// //Formas de definir variables.
+// var nombreVariable;
+// let nombreVariable2
+// let nombreVariable3 = 'Algo'
 
-let numero = 1000
-let boleano = true
-let myArray = [1, '2', true]
-let myObjectKoder = {
-    nombre = 'Elvi'
-}
+// let numero = 1000
+// let boleano = true
+// let myArray = [1, '2', true]
+// let myObjectKoder = {
+//     nombre = 'Elvi'
+// }
 
 const PI = 3.1416 //Los nombres de las constantes se deben escribir en mayúsculas.
 
 //Formas de escritura para el curso
 
-let name_koder = 'Jorge'
-const NAME_KODER = 'Oscar'
+let name_koder = prompt('¿cuál es tu nombre?')
+console.log(name_koder)
+
+let age_koder = prompt('Cuál es tu edad')
+console.log(age_koder)
+ console.log('suma:, age_koder + 5')
+
+ let resultSuma =parseInt(age_koder) + 5
+console.log()
+ console.log('suma', resultSuma)
+ console.log('multi: ', age_koder*5)
+ console.log('div:', age_koder/5)
+ console.log('rest:',age_koder - 5)
