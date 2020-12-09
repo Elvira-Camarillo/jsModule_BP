919841a2e71458844b64880807c26c3b7feba253
Elvira-Camarillo <gcamarilloluna@gmail.com>
2020-12-08 21:40:25 -0600

    ADD switch to weather_today:octocat:

--- class_two/js/weather_today.js
+++ class_two/js/weather_today.js
@@ -1,17 +1,36 @@
 console.log('Ejercicio 2: ¿cómo esta el clima hoy?')
 
+// if( weather_today === 'soleado' ) {
+//     console.log(`¡Hey koder!, es un buen día para ir a playa`)
+// } else if ( weather_today === 'lluvioso' ) {
+//     console.log(`¡Hey koder!, si vas a salir de casa no olvides tu sombrilla`)
+// } else if (  weather_today === 'nevando' ) {
+//     console.log(`Hey koder, es hora de esquiar :)`)
+// } else if ( weather_today === 'nublado' ) {
+//     console.log(`¡Hey koder!, te sugiero disfrutar de un rico cafecito o un chocolatito `)
+// } else {
+//     let weather_description = prompt('Describeme tu día por favor')
+//     console.log(`El koder dice: ${weather_description}`)
+// }
+
 let weather_today = prompt('¿cómo esta el día hoy?')
 weather_today = weather_today.toLocaleLowerCase()
 
-if( weather_today === 'soleado' ) {
-    console.log(`¡Hey koder!, es un buen día para ir a playa`)
-} else if ( weather_today === 'lluvioso' ) {
-    console.log(`¡Hey koder!, si vas a salir de casa no olvides tu sombrilla`)
-} else if (  weather_today === 'nevando' ) {
-    console.log(`Hey koder, es hora de esquiar :)`)
-} else if ( weather_today === 'nublado' ) {
-    console.log(`¡Hey koder!, te sugiero disfrutar de un rico cafecito o un chocolatito `)
-} else {
-    let weather_description = prompt('Describeme tu día por favor')
-    console.log(`El koder dice: ${weather_description}`)
+switch (weather_today) {
+    case 'soleado':
+        console.log(`¡Hey koder!, es un buen día para ir a playa`)
+        break
+    case 'lluvioso':
+        console.log(`¡Hey koder!, si vas a salir de casa no olvides tu sombrilla`)
+        break
+    case 'nevando':
+        console.log(`Hey koder, es hora de esquiar :)`)
+        break
+    case 'nublado':
+        console.log(`¡Hey koder!, te sugiero disfrutar de un rico cafecito o un chocolatito `)
+        break
+    default:
+        let weather_description = prompt('Describeme tu día por favor')
+        console.log(`El koder dice: ${weather_description}`)
+        break
 }
