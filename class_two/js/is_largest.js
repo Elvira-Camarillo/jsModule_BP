8377dd0720c184a86bcad80ffe8eb5383cf13e12
Elvira-Camarillo <gcamarilloluna@gmail.com>
2020-12-08 20:27:49 -0600

    MOD is_largest.js:octocat:

--- class_two/js/is_largest.js
+++ class_two/js/is_largest.js
@@ -2,9 +2,9 @@ console.log('¿Qué número es mayor?')
 const NUMBER_ONE = prompt('Dame un número por favor')
 const NUMBER_TWO = prompt('Escribe un segundo número')
 if (NUMBER_ONE > NUMBER_TWO) {
-    console.log(`El ${NUMBER_ONE} es mayor que ${NUMBER_TWO}`)
+    console.log(`El ${NUMBER_ONE} es el mayor`)
 } else if (NUMBER_ONE === NUMBER_TWO) {
     console.log(`El ${NUMBER_ONE} es igual a ${NUMBER_TWO}`)
 } else {
-    console.log(`El ${NUMBER_ONE} es menor que ${NUMBER_TWO}`)
-}
+    console.log(`El ${NUMBER_TWO} es el mayor`)
+} 
