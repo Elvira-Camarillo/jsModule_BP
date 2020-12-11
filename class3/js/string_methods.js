c5fe636929341e865869a5c7ad9766b140956217
Elvira-Camarillo <gcamarilloluna@gmail.com>
2020-12-10 19:50:29 -0600

    ADD class4:octocat:

--- class3/js/string_methods.js
+++
@@ -1,5 +0,0 @@
-// Method startsWith  --> Returns true or false
-// string.startsWith(searchvalue, start)
-
-var str = "Hello world, welcome to the universe.";
-var n = str.startsWith("world", 6);
---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Imprimir la tabla del 8 en reversa
+
+console.log('Ejercicio 1 tabla del 8 en reversa')
+let tabla8 = []
+for(let i = 1; i< 11; i++){
+    console.log(`8 x ${(11 - i)} = ${8 * (11 - i)}`)
+}
