0f18c875534e983f3679f1a04a4d3c12ff461d3f
Elvira-Camarillo <gcamarilloluna@gmail.com>
2020-12-14 22:32:26 -0600

    ADD Methods Arrays:octocat:

---
+++ js/array.js
@@ -0,0 +1,31 @@
+// Declarar un string
+const nameString = 'assss'
+
+// Array 
+// const myArrayOne = [ 'string', 2, [1,2,3]]
+// console.log(myArrayOne[1])
+// // Accede a la posición la posición 2 del array que esta dentro de myArray
+// console.log(myArrayOne[2][2]) 
+
+const myArray = [ 'string', 2, 3, 9, [1,23]]
+
+for(let i = 0; i < myArray.length; i ++ ){
+    console.log(myArray[i])
+}
+
+const arrayTables = [1,4,7]
+for (let count = 0; count < arrayTables.length; count ++){
+    let number = arrayTables[count]
+    for (let i = 1; i <= 10; i ++){
+        console.log(`La tabla ${number} x ${i} = ${number * i}`)
+    }
+}
+
+// Suma todos los elementos de un array
+
+const arraySumNumber = [1,2,3,4]
+let totalArraySum = 0
+for(let n = 0; n < arraySumNumber.length; n++){
+    totalArraySum += arraySumNumber[n]
+}
+console.log(totalArraySum)
