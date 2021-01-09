38c19efacfdf928dac192650cbbf57d9ba702c17
Elvira-Camarillo <gcamarilloluna@gmail.com>
2021-01-08 19:29:23 -0600

    MOD homework:octocat:

--- js/events.js
+++ js/events.js
@@ -1,40 +1,20 @@
-const filterKoderSelectAD = (arr) => {
-    let order = document.getElementById('order_list').value
-    let fakeArr = [...arr]
-    let orderArray = fakeArr.sort()
-    if (order === 'a') {
-        return orderArray
-    } else if (order === 'd'){
-        return orderArray.reverse()
-    } else {
-        return fakeArr
-    }
-}
-
-const filterKoderSelectAZ =( letter )=> {
-    let filtrados = kodersArr.filter( (koder) => {
-        return  koder.toLowerCase().startsWith(letter.toLowerCase())
+const kodersArr = ['Ale', 'ale2', 'alejandra','oscar','ruben', 'brian', 'brenda']
+console.log('Hola Koders event')
+// Exercise
+const addElements = (selector,numberElements, arrNames) => {
+    let listKoders = ''
+    arrNames.forEach((koder, index) => {
+        if(index < numberElements ) {
+            listKoders += `<li>Koder ${index + 1}: ${koder}</li>`
+        }
     })
-    return filtrados
+    document.querySelector(selector).innerHTML = listKoders
 }
 
-// const filterAZ = () => {
-//     let valorderaz = document.getElementById('orderLetter').value
-//     let arrarKoders = filterKoderSelectAZ(valorderaz ? valorderaz:'')
-//     let resultadoKoders = filterKoderSelectAD(arrarKoders)
-
-//     console.log(arrarKoders)
-//     console.log(resultadoKoders)
-
-//     addElements('#lista', resultadoKoders.length, resultadoKoders)
-// }
-const filterAZ = () => {
-    let valorderad = document.getElementById('order_list').value
-    console.log(valorderad)
-    let valorderaz = document.getElementById('orderletter').value
-    let resultadoKoders = filterKodersSelectAD(kodersArr)
-    console.log(resultadoKoders)
-    let arrayKoders = filterKodersSelectAZ(valorderad)
-    console.log(arrayKoders)
-    addElements('#lista', resultadoKoders.length , arrayKoders)
+// Exercise filter
+const filterKoders = (letter) => {
+    let filtrados = kodersArr.filter((koder)=> {
+        return koder.toLowerCase().startsWith(letter.toLowerCase())
+    })
+    addElements('#lista', filtrados.length , filtrados)   
 }
